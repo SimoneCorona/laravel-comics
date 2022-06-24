655c9be5941773b34a5b3783637a7ba663f2afa6
SimoneCorona <corona.simone93@gmail.com>
2022-06-24 17:18:14 +0200

    pre ticket again

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,107 @@
 <footer>
-    FOOTER
+    <section class="icons">
+        <div class="container">
+            <ul>
+                <li>
+                    <img src="{{ asset('img/buy-comics-merchandise.png') }}" alt="">
+                    <p>DC MERCHANDISE</p>
+                </li>
+                <li>
+                    <img src="{{ asset('img/buy-comics-subscriptions.png') }}" alt="">
+                    <p>SUBSCRIPTION</p>
+                </li>
+                <li>
+                    <img src="{{ asset('img/buy-comics-shop-locator.png') }}" alt="">
+                    <p>COMIC SHOP LOCATION</p>
+                </li>
+                <li>
+                    <img src="{{ asset('img/buy-dc-power-visa.svg') }}" alt="">
+                    <p>DC POWER VISA</p>
+                </li>
+            </ul>
+        </div>
+    </section>
 
+    <section class="banner">
+        <div class="container">
+            <div class="links-wrapper">
+                <div>
+                    <ul>
+                        <li>
+                            <h1>DC COMICS</h1>
+                        </li>
+                        <li><a href="">Characters</a></li>
+                        <li><a href="">Comics</a></li>
+                        <li><a href="">Movies</a></li>
+                        <li><a href="">TV</a></li>
+                        <li><a href="">Games</a></li>
+                        <li><a href="">Videos</a></li>
+                        <li><a href="">News</a></li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <h1>SHOP</h1>
+                        </li>
+                        <li><a href="">Shop DC</a></li>
+                        <li><a href="">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <ul>
+                        <li>
+                            <h1>DC</h1>
+                        </li>
+                        <li><a href="">Terms Of Use</a></li>
+                        <li><a href="">Privacy policy (New)</a></li>
+                        <li><a href="">Ad Choices</a></li>
+                        <li><a href="">Advertising</a></li>
+                        <li><a href="">Jobs</a></li>
+                        <li><a href="">Subscriptions</a></li>
+                        <li><a href="">Talent Workshops</a></li>
+                        <li><a href="">CPSC Certificates</a></li>
+                        <li><a href="">Ratings</a></li>
+                        <li><a href="">Shop Help</a></li>
+                        <li><a href="">Contact Us</a></li>
+                    </ul>
+                </div>
+                
+                <div>
+                    <ul>
+                        <li>
+                            <h1>SITES</h1>
+                        </li>
+                        <li><a href="">DC</a></li>
+                        <li><a href="">MAD Magazine</a></li>
+                        <li><a href="">DC Kids</a></li>
+                        <li><a href="">DC Universe</a></li>
+                        <li><a href="">DC Power Visa</a></li>
+                        
+                    </ul>
+                </div>
+            </div>
+
+            <div class="banner-log">
+                <img src="{{ asset('img/dc-logo-bg.png') }}" alt="">
+            </div>
+        </div>
+    </section>
+
+    <section class="footer-bottom">
+        <div class="container">
+            <div>
+                <span>
+                    <a href="">SIGN-UP NOW!</a> 
+                </span>
+            </div>
+            <div class="follow-us">
+                <h1>FOLLOW US</h1>
+                <img src="{{ asset('img/footer-facebook.png') }}" alt="">
+                <img src="{{ asset('img/footer-twitter.png') }}" alt="">
+                <img src="{{ asset('img/footer-youtube.png') }}" alt="">
+                <img src="{{ asset('img/footer-pinterest.png') }}" alt="">
+                <img src="{{ asset('img/footer-periscope.png') }}" alt="">
+            </div>
+        </div>
+    </section>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,23 @@
 <header>
-    HEADER
+    <div class="container">
+        <div class="header_logo">
+          <img src="{{ asset('img/dc-logo.png') }}" alt="">
+        </div>
+        
+        <nav>
+          <ul>
+            <li><a href=""> CHARACTERS </a></li>
+            <li class="active"><a href=""> COMICS </a></li>
+            <li><a href=""> MOVIES </a></li>
+            <li><a href=""> TV </a></li>
+            <li><a href=""> GAMES </a></li>
+            <li><a href=""> COLLECTIBLES </a></li>
+            <li><a href=""> VIDEOS </a></li>
+            <li><a href=""> FANS </a></li>
+            <li><a href=""> NEWS </a></li>
+            <li><a href=""> SHOP </a></li>
+          </ul>
+        </nav>
+      </div>
 
 </header>
